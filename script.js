753b59f7d438e7e4b4cf876faba487c4cdfc636e
unamednada <dias.gustavo.dias@gmail.com>
2021-07-22 11:20:35 -0300

    Adiciona função customHouseStyle

--- script.js
+++ script.js
@@ -11,3 +11,28 @@ const authLog = () => {
 };
 
 btnLogin.addEventListener('click', authLog);
+
+const house = document.querySelector('#house');
+
+function customHouseStyle() {
+  const houseValue = house.value;
+  const fieldBorder = document.querySelector('.field-border');
+  switch (houseValue) {
+  case 'Gitnória':
+    fieldBorder.style.border = '1px solid red';
+    break;
+  case 'Reactpuff':
+    fieldBorder.style.border = '1px solid blue';
+    break;
+  case 'Corvinode':
+    fieldBorder.style.border = '1px solid green';
+    break;
+  case 'Pytherina':
+    fieldBorder.style.border = '1px solid yellow';
+    break;
+  default:
+    break;
+  }
+}
+
+house.addEventListener('change', customHouseStyle);
